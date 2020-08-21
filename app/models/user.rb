c4f78b3a9867632c06c1cd9098c39d405a9a2cc4
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-08-21 18:03:56 +0900

    ユーザー管理機能修正_1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :name_first, :name_last, :name_kana_first, :name_kana_last, :birthday, presence: true
-  validates :email, uniqueness: true
-  validates :name_first, :name_last, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :name_kana_first, :name_kana_last, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,100}+\z/ }
+  regexp_name = /\A[ぁ-んァ-ン一-龥]/
+  regexp_name_kana = /\A[ァ-ヶー－]+\z/
+  regexp_password = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,100}+\z/
+
+  with_options presence: true do
+    validates :nickname, :birthday
+    validates :email, uniqueness: true
+    validates :name_first, :name_last, format: { with: regexp_name }
+    validates :name_kana_first, :name_kana_last, format: { with: regexp_name_kana }
+    validates :password, format: { with: regexp_password }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "items#index"
-  # resources :users
 
   post "/users/sign_up", to: "users#create"
 end
