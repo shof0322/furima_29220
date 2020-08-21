dc926965b224e82cf22779b049b58b3adb13d99f
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-08-21 11:54:24 +0900

    新規登録・ログイン・ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :name_first, :name_last, :name_kana_first, :name_kana_last, :birthday, presence: true;
+  validates :name_first, :name_last, format: {with: /\A[ぁ-んァ-ン一-龥]/};
+  validates :name_kana_first, :name_kana_last, format: {with: /\A[ァ-ヶー－]+\z/};
+  
 end
