ee7c825cfd31dd3f72d3facf11785e2255a56431
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-08-27 11:51:00 +0900

    商品出品機能の修正_1
    ・不要なコメントアウト消去
    ・price.jsの変数定義変更

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,8 +1,8 @@
 $(function () {
   $( document ).on( 'change', 'input', function(){ 
-    var price = $( this ).val();
-    var tax = price / 10;
-    var profit = price - tax
+    const price = $( this ).val();
+    const tax = price / 10;
+    const profit = price - tax
     $('#add-tax-price').empty();
     $('#profit').empty();
     $('#add-tax-price').append(tax);
