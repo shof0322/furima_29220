c292f550bc93f8d59b5a532846850ce85eab2c0d
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-09-01 21:48:39 +0900

    rails db fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_111241) do
+ActiveRecord::Schema.define(version: 2020_08_29_021351) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
